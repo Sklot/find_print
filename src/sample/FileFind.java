a91e3416b4ba478f96ab060db04ff542ca86fc5c
Denis Ryskal <denis.ryskal@gmail.com>
2019-02-07 00:37:59 +0300

    Huge update

--- src/sample/FileFind.java
+++ src/sample/FileFind.java
@@ -5,39 +5,65 @@ import java.util.Scanner;
 
 public class FileFind {
     String[] fileList;
+    String file_path;
 
     public FileFind() {
         Settings sett = new Settings();
         File file = new File(sett.getPath());
+        file_path = sett.getPath();
         this.fileList = file.list();
     }
 
     void find_by_name(String string, String[] data) {
         int f = 0;
         for (String s : data
-                ) {
+        ) {
             if (string.toLowerCase().equals(s))
                 f++;
         }
         if (f > 0) {
             System.out.println("File: " + string + " найден.");
-        } else System.out.println("Файл с таким именем не найден.");;
+        } else System.out.println("Файл с таким именем не найден.");
     }
 
     void start_search() {
         Scanner scanner = new Scanner(System.in);
         String s;
         System.out.println("Для выхода из поиска введите !help");
-        System.out.println("Начинаем поиск файлов." +'\n' + "Ввведите имя файла:");
-        while((s = scanner.nextLine())!=null)
-//            String s = scanner.nextLine();
-            if(s.equals("!help")){
+        System.out.println("Для списка всех файлов введите !list");
+        System.out.println("Начинаем поиск файлов." + '\n' + "Ввведите имя файла:");
+        while ((s = scanner.nextLine()) != null)
+            if (s.equals("!help")) {
                 System.out.println("Выход из поиска.");
                 break;
-            }else {
+            } else if (s.equals("!list")) {
+
+                show_all_list(fileList, file_path);
+
+
+            } else {
                 find_by_name(s, fileList);
                 System.out.println("Ввведите имя файла:");
             }
+    }
+
+
+    void show_all_list(String[] file_data, String file_path) {
+        String start_path = file_path;
+        for (int i = 0; i < file_data.length; i++) {
+            File f = new File(start_path + "\\" + file_data[i]);
+            if (f.isDirectory()) {
+                System.out.println("dir " + file_data[i]);
+                System.out.println("|");
+                String[] files = f.list();
+                show_all_list(files, f.getPath());
+
+            } else if (f.isFile()) {
+                System.out.println(file_data[i]);
+            }
+
+        }
+
     }
 
 
--- src/sample/Settings.java
+++ src/sample/Settings.java
@@ -1,29 +1,61 @@
 package sample;
 
-import java.io.File;
+import java.io.*;
+import java.util.Scanner;
 
-public class Settings {
+class Settings {
     private String path;
 
     public String getPath() {
         return path;
     }
 
-    public void setPath(String path) {
+    private void setPath(String path) {
         this.path = path;
     }
 
     public Settings() {
-
-    }
-
-    public static boolean isCreate(){
-        File file = new File("src/Files/settings.txt");
+        File file = new File("src/settings.txt");
         if (file.exists()) {
-            return true;
+            try {
+                FileReader fr = new FileReader(file);
+                BufferedReader reader = new BufferedReader(fr);
+                try {
+                    String path = reader.readLine();
+                    if (path != null) {
+                        setPath(path);
+                    } else writePath(file);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            writePath(file);
         }
-        return false;
     }
+
+    private void writePath(File file) {
+        try {
+            FileWriter writer = new FileWriter(file);
+            System.out.println("Введите адрес папки:");
+            Scanner scanner = new Scanner(System.in);
+            String path = scanner.nextLine();
+            writer.write(path);
+            setPath(path);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 
 }
